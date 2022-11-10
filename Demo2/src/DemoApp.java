266d9528c202735df5bb608e4087d10b8cdc75fb
Boris <berzerker6662@gmail.com>
2022-11-10 19:47:11 +0100

    First Commit

---
+++ Demo2/src/DemoApp.java
@@ -0,0 +1,17 @@
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+
+		int[] a = {10,20,30,40};
+		for(int elem : a) {
+			System.out.println(elem);
+		}
+		
+		String name = "sachin";
+		int count = name.toUpperCase().length();
+		System.out.println(count);
+
+	}
+
+}
