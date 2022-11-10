a97e9b331569226d414bf0372546109ba78a4860
berze <berze@LAPTOP-0RFKTNBR>
2022-11-10 20:50:08 +0100

    Second Commit

--- Demo3/src/Test.java
+++ Demo3/src/Test.java
@@ -3,6 +3,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		StringBuffer sb = new StringBuffer("boris");
+		int length = sb.append("IHDDSD").reverse().length();
+		System.out.println(length);
 
 	}
 
